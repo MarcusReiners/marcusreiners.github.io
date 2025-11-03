d0c6fb61927328fed5ca85d7e3e1d9546bf54983
Marcus Reiners <marcus@qfs.de>
2025-11-03 21:49:52 +0100

    deploy

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import Portfolio from './Portfolio'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Portfolio />
-  </React.StrictMode>,
-)
+    <BrowserRouter basename="/marcusreiners.github.io/">
+      <Routes>
+        <Route path="*" element={<Portfolio />}/>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
